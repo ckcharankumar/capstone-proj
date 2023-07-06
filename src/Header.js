fb6845c83dbdf062e2e1e2212fdea78c7f09067a
ckcharankumar <ckcharankumaravel1@gmail.com>
2023-07-06 21:05:27 +0530

    semantic structure and OGP done

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Header () {
+return (
+    <header>
+        <meta name="description" content="Little Lemon Restaurant offers a delightful fusion of flavors using fresh, locally sourced ingredients, providing a memorable and exquisite dining experience."/>
+        <meta property="og:title" content="Lemon Resturant"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:image" content="Lemon Resturant"/>
+        <meta property="og:url" content="https://littlelemon"/>
+        <meta property="og:description" content="Little Lemon Restaurant offers a delightful fusion of flavors using fresh, locally sourced ingredients, providing a memorable and exquisite dining experience."/>
+        <meta property="og:locale" content="en_US"/>
+        <meta property="og:site_name" content="Little Lemon"/>
+
+        <img src = "" alt = "Little Lemon logo" />
+
+        </header>
+)
+
+}
+
+export default Header;
---
+++ src/Nav.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Nav(){
+    return(
+        <nav>
+            <ul>
+                <li>
+                    < a href="www.facebook.com">Home</a>
+                </li>
+                <li>
+                    < a href="www.facebook.com">About</a>
+                </li>
+                <li>
+                    < a href="www.facebook.com">Menu</a>
+                </li>
+                <li>
+                    < a href="www.facebook.com">Reservation</a>
+                </li>
+                <li>
+                    < a href="www.facebook.com">Order Online</a>
+                </li>
+                <li>
+                    < a href="www.facebook.com">Login</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
