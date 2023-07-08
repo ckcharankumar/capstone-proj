6d505166abefed5bbc617398d279bbaf47170708
ckcharankumar <ckcharankumaravel1@gmail.com>
2023-07-08 22:27:12 +0530

    new

--- src/Main.js
+++ src/Main.js
@@ -20,6 +20,11 @@ function Main() {
                 <img src={require("./icons_assets/restauranfood.jpg")} alt="Hero image" className="heroimg" />
                 </div>
         </div>
+        <div className="topic-button">
+            <h1 id="menutopic" >This weeks specials!</h1>
+            <button type="Submit" value={SubmitEvent}>Online Menu</button>
+
+        </div>
             </>
     );
 }
