ffd953e6d85956fd30bfc44a3d748db0374b3990
ckcharankumar <ckcharankumaravel1@gmail.com>
2023-07-08 21:49:40 +0530

    first section addded

--- src/Main.js
+++ src/Main.js
@@ -3,19 +3,24 @@ import "./Main.css";
 
 function Main() {
     return (
+        <>
         <div className="main-container">
             <div className="hero-container">
-            <h1>Little Lemon</h1>
-            <h2>Chicago</h2>
-            <p>We are a family owned
-                Mediterranean resturant,
-                focused on tradtional
-                recepies served with a modern
-                twist
-            </p>
-            <button type="Submit" value={SubmitEvent}>Reserve a Table</button>
+                <h1>Little Lemon</h1>
+                <h2>Chicago</h2>
+                <p>We are a family owned
+                    Mediterranean resturant,
+                    focused on tradtional
+                    recepies served with a modern
+                    twist
+                </p>
+                <button type="Submit" value={SubmitEvent}>Reserve a Table</button>
             </div>
+            <div>
+                <img src={require("./icons_assets/restauranfood.jpg")} alt="Hero image" className="heroimg" />
+                </div>
         </div>
+            </>
     );
 }
 
