19f3b7c5cae77c20290265b7b672dc480bc9c7a3
ckcharankumar <ckcharankumaravel1@gmail.com>
2023-07-16 17:19:37 +0530

    main form finished

--- src/BookingForm.js
+++ src/BookingForm.js
@@ -1,27 +1,42 @@
 import React, { useState } from "react";
 import "./BookingForm.css";
 
-function BookingForm() {
+function BookingForm({ availableTimes, updateTimes }) {
   const [name, setName] = useState("");
-const [contactNo, setContactNo] = useState("");
-const [date, setDate] = useState();
-const [guests, setGuests] = useState(1);
-const [time, setTime] = useState("17:00");
-const [occasion, setOccasion] = useState("Birthday");
+  const [contactNo, setContactNo] = useState("");
+  const [date, setDate] = useState("");
+  const [guests, setGuests] = useState(1);
+  const [time, setTime] = useState("17:00");
+  const [occasion, setOccasion] = useState("Birthday");
 
-const availableTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+  const handleDateChange = (e) => {
+    const selectedDate = e.target.value;
+    setDate(selectedDate);
+    updateTimes(selectedDate);
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    function handleSubmit(e) {
-        e.preventDefault();
+    if (!name || !contactNo || !date || !time) {
+      alert("Please fill in all required fields.");
+      return;
+    }
 
-        if (!name || !contactNo || !date || !time) {
-            alert("Please fill in all required fields.");
-            return;
-        }
+    console.log("Successfully Booked");
+    alert("Successfully Booked");
+    resetForm();
 
-        console.log("Successfully Booked");
-    };
+  };
+
+  const resetForm = () => {
+    setName("");
+    setContactNo("");
+    setDate("");
+    setGuests(1);
+    setTime("17:00");
+    setOccasion("Birthday");
+  };
 
   return (
     <div className="containerf">
@@ -41,41 +56,43 @@ const availableTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
 
           <div className="field">
             <label>Choose date:</label>
-            <input type="date"
-            value={date}
-            onChange={(e) => setDate (e.target.value)}
+            <input
+              type="date"
+              value={date}
+              onChange={handleDateChange}
             />
           </div>
 
           <div className="field">
             <label>Choose time:</label>
             <select
-          id="res-time"
-          value={time}
-          onChange={(e) => setTime(e.target.value)}
-        >
-          {availableTimes.map((time) => (
-            <option key={time}>{time}</option>
-          ))}
-        </select>
-        </div>
-        
+              id="res-time"
+              value={time}
+              onChange={(e) => setTime(e.target.value)}
+            >
+              {availableTimes.map((time) => (
+                <option key={time}>{time}</option>
+              ))}
+            </select>
+          </div>
 
           <div className="field">
             <label>No of Guests:</label>
-            <input type="number"
-             placeholder="1"
-              min="1" max="10" 
-              value={guests} 
-              onChange={(e)=> setGuests(parseInt(e.target.value))}
-              />
+            <input
+              type="number"
+              placeholder="1"
+              min="1"
+              max="10"
+              value={guests}
+              onChange={(e) => setGuests(parseInt(e.target.value))}
+            />
           </div>
 
           <div className="field">
             <label>Occasion:</label>
-            <select 
-            value={occasion}
-            onChange={(e) => setOccasion(e.target.value)}
+            <select
+              value={occasion}
+              onChange={(e) => setOccasion(e.target.value)}
             >
               <option>Meet up</option>
               <option>Birthday</option>
@@ -94,7 +111,14 @@ const availableTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
             />
           </div>
 
-          <button type="submit" value="Make your reservation" className="subbutton">Reserve Table</button>
+          <button
+            type="submit"
+            value="Make your reservation"
+            className="subbutton"
+          >
+
+            Reserve Table
+          </button>
         </fieldset>
       </form>
     </div>
@@ -102,8 +126,3 @@ const availableTimes = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
 }
 
 export default BookingForm;
-
-
-
-
-
--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./Main.css";
 import Card from "./Card";
 import Banner from "./Banner";
 import Topic from "./Topic";
+import BookingForm from "./BookingForm";
 
+
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "UPDATE_TIMES":
+      return ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+    default:
+      return state;
+  }
+}
 
 function Main() {
+  const [availableTimes, dispatch] = useReducer(reducer, ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]);
+  
+  const updateTimes = (date) => {
+    dispatch({ type: "UPDATE_TIMES", date });
+  };
+
+
+
     return (
       <div>
         <Banner
@@ -39,8 +58,8 @@ function Main() {
             description="This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined. "
           />
 
-          
         </div>
+        <BookingForm availableTimes={availableTimes} updateTimes={updateTimes} />
       </div>
     );
   }
